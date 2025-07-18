dbd18fc4a39e617efa4272c050214516c24cbe52
Ahmettcakar <116648416+Ahmettcakar@users.noreply.github.com>
2025-07-18 13:13:15 +0300

    logger middleware added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
 import { TodosService } from './todos/todos.service';
-
+import { LoggerMiddleware } from './logger.middleware';
 @Module({
   imports: [],
   controllers: [AppController, TodosController],
   providers: [AppService, TodosService],
+  
+
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*'); // Apply LoggerMiddleware to all routes
+  }
+}
+
